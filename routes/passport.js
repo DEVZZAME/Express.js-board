3588eff28dd4973febde87c83657bbc36426c8e5
ZZAME <92978598+DEVZZAME@users.noreply.github.com>
2022-09-26 13:09:08 +0900

    modify passport.js

--- routes/passport.js
+++ routes/passport.js
@@ -1,6 +1,11 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const NaverStrategy = require('passport-naver').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const KakaoStrategy = require('passport-kakao').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+
+const verifyModule = require('./resister').verifyPassword;
 
 // require('dotenv').config();
 const mongoClient = require('./mongo');
@@ -18,7 +23,15 @@ module.exports = () => {
         const idResult = await userCursor.findOne({ id: id });
 
         if (idResult !== null) {
-          if (idResult.pw === pw) {
+          if (idResult.salt !== undefined) {
+            console.log(idResult.pw);
+            const passwardResult = verifyModule(pw, idResult.salt, idResult.pw);
+            if (passwardResult) {
+              cb(null, idResult);
+            } else {
+              cb(null, false, { message: '비밀번호가 틀렸습니다' });
+            }
+          } else if (idResult.pw === pw) {
             cb(null, idResult);
           } else {
             cb(null, false, { message: '비밀번호가 틀렸습니다' });
@@ -59,6 +72,110 @@ module.exports = () => {
             cb(null, newNaverUser);
           } else {
             cb(null, false, { message: '네이버 회원 생성 에러' });
+          }
+        }
+      }
+    )
+  );
+
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: process.env.GOOGLE_CB_URL,
+      },
+      async (accessToken, refreshToken, profile, cb) => {
+        console.log(profile);
+        const client = await mongoClient.connect();
+        const userCursor = client.db('kdt1').collection('users');
+        const result = await userCursor.findOne({ id: profile.id });
+        console.log(profile);
+        if (result !== null) {
+          cb(null, result);
+        } else {
+          const newNaverUser = {
+            id: profile.id,
+            name:
+              profile.displayName !== undefined
+                ? profile.displayName
+                : profile.emails[0].value,
+            provider: profile.provider,
+          };
+          const dbResult = await userCursor.insertOne(newNaverUser);
+          if (dbResult.acknowledged) {
+            cb(null, newNaverUser);
+          } else {
+            cb(null, false, { message: '회원 생성 에러' });
+          }
+        }
+      }
+    )
+  );
+
+  passport.use(
+    new FacebookStrategy(
+      {
+        clientID: process.env.FACEBOOK_CLIENT,
+        clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+        callbackURL: process.env.FACEBOOK_CB_URL,
+      },
+      async (accessToken, refreshToken, profile, cb) => {
+        console.log(profile);
+        const client = await mongoClient.connect();
+        const userCursor = client.db('kdt1').collection('users');
+        const result = await userCursor.findOne({ id: profile.id });
+        console.log(profile);
+        if (result !== null) {
+          cb(null, result);
+        } else {
+          const newNaverUser = {
+            id: profile.id,
+            name:
+              profile.displayName !== undefined
+                ? profile.displayName
+                : profile.emails[0].value,
+            provider: profile.provider,
+          };
+          const dbResult = await userCursor.insertOne(newNaverUser);
+          if (dbResult.acknowledged) {
+            cb(null, newNaverUser);
+          } else {
+            cb(null, false, { message: '회원 생성 에러' });
+          }
+        }
+      }
+    )
+  );
+
+  passport.use(
+    new KakaoStrategy(
+      {
+        clientID: process.env.KAKAO_ID, // 카카오 로그인에서 발급받은 REST API 키
+        callbackURL: process.env.KAKAO_URL, // 카카오 로그인 Redirect URI 경로
+      },
+      async (accessToken, refreshToken, profile, cb) => {
+        console.log(profile);
+        const client = await mongoClient.connect();
+        const userCursor = client.db('kdt1').collection('users');
+        const result = await userCursor.findOne({ id: profile.id });
+        console.log(profile);
+        if (result !== null) {
+          cb(null, result);
+        } else {
+          const newNaverUser = {
+            id: profile.id,
+            name:
+              profile.displayName !== undefined
+                ? profile.displayName
+                : profile.emails[0].value,
+            provider: profile.provider,
+          };
+          const dbResult = await userCursor.insertOne(newNaverUser);
+          if (dbResult.acknowledged) {
+            cb(null, newNaverUser);
+          } else {
+            cb(null, false, { message: '회원 생성 에러' });
           }
         }
       }
